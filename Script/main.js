acb7f1c0696901003bde3092e4b3e84341cc7f32
DanielArg100 <danicaffe100@gmail.com>
2026-04-15 15:12:12 -0300

    15/4/2026 👍

--- Script/main.js
+++ Script/main.js
@@ -3,8 +3,8 @@ setTimeout(() => {
     document.getElementById("gif").classList.add("Cafferata")
     setTimeout(() => {
         location.href="Pages/Home.html"
-    }, 1500);
-}, 2400);
+    }, 800);
+}, 1700);
 
 // todo ---> 1700 Tiempo Total
 // todo ---> 800 Tiempo redirección
